0f1b72d76991e3d0d9ad467348ffe53c0a4f6202
tobiaszgithub <tobiasz.github@gmail.com>
2016-03-26 13:03:29 +0100

    Inital commit for exporting project to digitalocean

---
+++ README.md
@@ -0,0 +1,16 @@
+# test-driven-development-with-python
+To run examples:
+
+1. Install Python 3
+2. Create virtual env:
+  1. $ pyvenv /path/to/new/virtual/environment
+  2. to activate virtual env: 
+    1. $ cd /path/to/new/virtual/environment
+    2. $ Scripts/activate
+  3. to deactivate virtual env:
+    1. $ Scripts/deactivate
+3. In virtual env install following libraries:
+  1. $ pip install Django==1.9.4
+  2. $ pip install --upgrade selenium
+4. Download chromedriver from https://sites.google.com/a/chromium.org/chromedriver/downloads
+5. Put chromedriver.exe to /path/to/new/virtual/environment/Scripts
---
+++ lists/tests.py
@@ -0,0 +1,122 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+from django.template.loader import render_to_string
+from lists.models import Item, List
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertTrue(response.content.decode(), expected_html)
+        
+        #=======================================================================
+        # self.assertIn('A new list item',response.content.decode())
+        # expected_html = render_to_string('home.html', {
+        #                 'new_item_text': request.POST.get('item_text', ''),
+        #                 }, request=request)
+        # self.assertEqual(response.content.decode(), expected_html)
+        #=======================================================================
+
+    
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/'%(list_.id,))
+        self.assertTemplateUsed(response, 'list.html')
+    
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/'%(correct_list.id,))   
+        self.assertEqual(response.context['list'], correct_list) 
+    
+    def test_display_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text="item 1", list=correct_list)
+        Item.objects.create(text="item 2", list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+        
+        response = self.client.get('/lists/%d/'%(correct_list.id))
+        
+        self.assertContains(response, "item 1" )
+        self.assertContains(response, "item 2" )    
+        self.assertNotContains(response, "other list item 1")
+        self.assertNotContains(response, "other list item 2")
+        
+    def test_saving_a_POST_request(self):
+        
+        self.client.post('/lists/new', data={'item_text':'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        newItem = Item.objects.first()
+        self.assertEqual(newItem.text, 'A new list item')
+        
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', 
+                                    data={'item_text':'A new list item'})
+        new_list = List.objects.first()
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/lists/%d/'%(new_list.id,))
+    
+    
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.post('/lists/%d/add_item'%(correct_list.id,),
+                                    data={'item_text':'A new item for an existing list'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(correct_list, new_item.list)
+        
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.post('/lists/%d/add_item'%(correct_list.id,),
+                        data={'item_text':'A new item for an existing list'})
+        self.assertRedirects(response, '/lists/%d/'%(correct_list.id,))
+        
+        
+class ListAndItemModelsTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+        
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item' )
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second' )
+        self.assertEqual(second_saved_item.list, list_ )
+
+        
+        
